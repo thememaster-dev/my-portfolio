5c57d21672ca20fce82205e0a3fe5534640895f1
Ariful islam <arifulbgt4@gmail.com>
2021-02-25 14:53:24 +0600

    feat: page routes

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import { Switch, Route } from 'react-router-dom';
+
+import Fallback from 'src/components/Fallback';
+import NotFound from '../404';
+
+const Home = lazy(() => import('../Home'));
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <Container maxWidth='sm'>
+      <Box my={4}>
+        <Suspense fallback={<Fallback />}>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </Box>
+    </Container>
+  );
 };
 
 export default App;
