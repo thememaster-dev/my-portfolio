d8157a993e41d6376ceb7a4ac8356e3f8337e901
Ariful islam <arifulbgt4@gmail.com>
2021-02-25 21:33:32 +0600

    fix: container wrapper

--- src/views/App/index.js
+++ src/views/App/index.js
@@ -1,24 +1,34 @@
 import React, { Suspense, lazy } from 'react';
 import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
 import { Switch, Route } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 import Fallback from 'src/components/Fallback';
 import NotFound from '../404';
 
 const Home = lazy(() => import('../Home'));
 
+const useStyle = makeStyles((theme) => ({
+  container: {
+    paddingTop: theme.spacing(1.5),
+    paddingBottom: theme.spacing(1.5),
+  },
+}));
+
 const App = () => {
+  const classes = useStyle();
+
   return (
-    <Container maxWidth='sm'>
-      <Box my={4}>
+    <Container maxWidth='md' className={classes.container}>
+      <Grid container spacing={3}>
         <Suspense fallback={<Fallback />}>
           <Switch>
             <Route exact path='/' component={Home} />
             <Route component={NotFound} />
           </Switch>
         </Suspense>
-      </Box>
+      </Grid>
     </Container>
   );
 };
