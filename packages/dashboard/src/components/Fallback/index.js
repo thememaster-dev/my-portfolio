1de33ba85b842807689a77263420ee415787c8df
Ariful islam <arifulbgt4@gmail.com>
2021-03-10 00:55:36 +0600

    feat: project list

--- packages/dashboard/src/components/Fallback/index.js
+++ packages/dashboard/src/components/Fallback/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Spin from 'antd/es/spin';
 import 'antd/es/spin/style/css';
 
-import './style.scss';
-
 const Fallback = () => {
   return (
     <div className='fallback'>
--- packages/dashboard/src/views/App/index.js
+++ packages/dashboard/src/views/App/index.js
@@ -5,6 +5,7 @@ import Fallback from 'src/components/Fallback';
 import Layout from 'src/containers/Layout';
 
 const Home = lazy(() => import('../Home'));
+const ProjectList = lazy(() => import('../ProjectList'));
 
 const App = () => {
   return (
@@ -14,6 +15,9 @@ const App = () => {
           <Switch>
             <Route exact path='/'>
               <Home />
+            </Route>
+            <Route exact path='/projects'>
+              <ProjectList />
             </Route>
           </Switch>
         </Suspense>
