d7a6d8c8cff34af66f7b5dbf046531ab799dca73
Ariful islam <arifulbgt4@gmail.com>
2021-03-09 16:33:21 +0600

    chore(clean): dashboard configure

--- packages/dashboard/src/index.js
+++ packages/dashboard/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from 'src/views/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
