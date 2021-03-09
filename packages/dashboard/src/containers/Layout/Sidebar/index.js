f7ada48ebd3ebad76a4b5b5239b9f1de71a36b2e
Ariful islam <arifulbgt4@gmail.com>
2021-03-10 00:33:37 +0600

    feat: logo in dashboard

--- packages/dashboard/src/containers/Layout/Sidebar/index.js
+++ packages/dashboard/src/containers/Layout/Sidebar/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Layout from 'antd/lib/layout';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const { Sider } = Layout;
 
 const Sidebar = () => {
   const collapsed = useSelector((state) => state.layout.collapsed);
 
-  return <Sider trigger={null} collapsible collapsed={collapsed}></Sider>;
+  return (
+    <Sider trigger={null} collapsible collapsed={collapsed}>
+      <Link className='logo' to='/'>
+        {collapsed ? 'P' : 'Portfolio'}
+      </Link>
+    </Sider>
+  );
 };
 
 export default Sidebar;
