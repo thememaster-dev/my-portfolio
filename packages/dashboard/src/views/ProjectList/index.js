57204e2ad987cc0fe32963537a5d35f990195474
Ariful islam <arifulbgt4@gmail.com>
2021-03-10 01:27:52 +0600

    feat: project card

--- packages/dashboard/src/views/ProjectList/index.js
+++ packages/dashboard/src/views/ProjectList/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import Card from 'antd/lib/card';
+import 'antd/lib/card/style/css';
+import Button from 'antd/lib/button';
+import 'antd/lib/button/style/css';
+import Row from 'antd/lib/row';
+import Col from 'antd/lib/col';
+
+import Project from 'src/components/Project';
 
 const ProjectList = () => {
-  return <div>ProjectList</div>;
+  return (
+    <Card
+      title='My Projects'
+      extra={<Button type='primary'>Create a new project</Button>}
+    >
+      <Row gutter={[24, 24]}>
+        <Col className='gutter-row' span={6}>
+          <Project image='' title='Hello project' link='xdfsdv' />
+        </Col>
+      </Row>
+    </Card>
+  );
 };
 
 export default ProjectList;
