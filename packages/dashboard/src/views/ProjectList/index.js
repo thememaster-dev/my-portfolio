d92769f29016010385e98827940fb0f161a015e6
Ariful islam <arifulbgt4@gmail.com>
2021-03-10 01:36:24 +0600

    feat: projects pagination

--- packages/dashboard/src/views/ProjectList/index.js
+++ packages/dashboard/src/views/ProjectList/index.js
@@ -4,7 +4,11 @@ import 'antd/lib/card/style/css';
 import Button from 'antd/lib/button';
 import 'antd/lib/button/style/css';
 import Row from 'antd/lib/row';
+import 'antd/lib/row/style/css';
 import Col from 'antd/lib/col';
+import 'antd/lib/col/style/css';
+import Pagination from 'antd/lib/pagination';
+import 'antd/lib/pagination/style/css';
 
 import Project from 'src/components/Project';
 
@@ -14,9 +18,20 @@ const ProjectList = () => {
       title='My Projects'
       extra={<Button type='primary'>Create a new project</Button>}
     >
-      <Row gutter={[24, 24]}>
-        <Col className='gutter-row' span={6}>
+      <Row gutter={[24, 24]} style={{ justifyContent: 'center' }}>
+        <Col span={6}>
           <Project image='' title='Hello project' link='xdfsdv' />
+        </Col>
+        <Col span={6}>
+          <Project image='' title='Hello project' link='xdfsdv' />
+        </Col>
+        <Col span={24}>
+          <Pagination
+            style={{ textAlign: 'center', marginTop: 20 }}
+            defaultCurrent={6}
+            showSizeChanger={false}
+            total={500}
+          />
         </Col>
       </Row>
     </Card>
