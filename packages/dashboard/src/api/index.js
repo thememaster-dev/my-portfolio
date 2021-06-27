0af8d80dfaf9241532a03cd2a80b906b44808d9c
Ariful islam <arifulbgt4@gmail.com>
2021-06-27 21:13:43 +0600

    FEAT: get published project axios

--- packages/dashboard/src/api/index.js
+++ packages/dashboard/src/api/index.js
@@ -24,6 +24,10 @@ export const createProject = (body) => {
   return axios.post(`${API}/project`, body, headers);
 };
 
+export const getPublishedPeoject = (page = 1) => {
+  return axios.get(`${API}/project/published/page/${page}`);
+};
+
 export const getUnpublishedPeoject = (page = 1) => {
   return axios.get(`${API}/project/unpublished/page/${page}`, headers);
 };
